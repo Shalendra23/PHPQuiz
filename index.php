48d873e40169f34b0bca3d0e67eb234860259cde
Shalendra Singh <singh.shalendra@gmail.com>
2019-03-24 09:47:20 +0200

    Upadated code for marking and auto grading when timer is complete

--- index.php
+++ index.php
@@ -7,16 +7,7 @@
     <link rel="stylesheet" href="css/style.css">
     <title>PHP 20 Question Quiz - CodeSpace Project</title>
 
-    <style>
-    input[type="submit"] {
-        background-color: orange;
-            border: none;
-            text-decoration: none;
-            color: white;
-            padding: 20px 20px;
-            margin: 20px 20px;
-            cursor: pointer;
-}</style>
+ 
 </head>
 <body> <!--onclick="printID(this.name);"-->
 
@@ -29,30 +20,35 @@ The above copyright notice and this permission notice shall be included in all c
 
 THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE. 
 -->
-<div class="timer">
-   <time id="countdown">5:00</time>
+<div class="timer" id="myTimer">
+    Time Remaining to complete the Quiz : 
+        <time id="countdown">5:00</time>
 </div>
 
+<div class="heading" id="myHeading">
+    <h1> Welcome to my PHP Quiz , I hope you enjoy it and 'Ace the Test'</h1>
+</div>
 
-<form action= "<?php echo $_SERVER['PHP_SELF']; ?>" method="POST">
+<img name="quizImage" id="imageid">
+
+<form name="quizform" action= "<?php echo $_SERVER['PHP_SELF']; ?>" method="POST">
 <?php
-
 
 // the 2d array taht contains the questions , options and choices for the quiz
 
-$questions=array(array('What is 1+0?', '1', '2', '3', '4', '1'));
-                // array('What is 1+1?', '1', '2', '3', '4', '2'),
-                // array('What is 1+2?', '1', '2', '3', '4', 'c'),  
-                // array('What is 1+3?', '1', '2', '3', '4', 'd'));
+$questions=array(array('What is 1+0?', '1', '2', '3', '4', '1'),
+                array('What is 1+1?', '1', '2', '3', '4', '2'),
+                array('What is 1+2?', '1', '2', '3', '4', '3'),  
+                array('What is 1+3?', '1', '2', '3', '4', '4'));
+
+$score = 0;                
 
 
-        //if(isset($_REQUEST[]))
-
-              for($row=0; $row<count($questions); $row++){
+               for($row=0; $row<count($questions); $row++){
                 echo "<h2>".$questions[$row][0]."</h2>"; // displays the questions form the 2d array called questions
                   for($colum=1; $colum<(count($questions[0]))-1; $colum++){
                         $ques = $questions[$row][$colum];
-                        echo '<input type="radio" value="'.$questions[$row][$colum].'" name="btn_'.$row.'" id="btn_'.$row.$colum.'">'.$ques.'</input>'; // creates the answer buttons for the questiosn
+                        echo '<span><input type="radio" value="'.$questions[$row][$colum].'" name="btn_'.$row.'" id="btn_'.$row.$colum.'">'.$ques.'</input></span>'; // creates the answer buttons for the questiosn
 
                         
                         // if ($colum % 2 == 0) // displays ony 2 buttons per line
@@ -68,50 +64,75 @@ $questions=array(array('What is 1+0?', '1', '2', '3', '4', '1'));
                         $choice = NULL;
                     }
 
-                    $answer = $questions[0][5];
-                    echo "$answer";
-
+                  $answer = $questions[$row][5];
+               
                           if ($choice != NULL){
                         if ($choice != $answer)
                         {
-                            echo "incorrect . $choice ";
+                           echo "<input size='50' value='Incorret, the correct answer is $choice' style='background-color: red;'>";
                         }else{
-                            echo"correct . $choice";
+                            $score++;
+                            echo "<input size='50' value='Correct $choice was the correct answer' style='background-color: green;'>";
                         }
                     }else{
-                            echo "please enter choice . $choice";
+                            echo "<input size='50' value='No Answer Selected' style='background-color: gray;'>";
+                    
                             
-                    }     
+                    }  
+
+                 }
+
+                        // calls calcScore function on click of the submit for grading function
+
+                        
+ 
+                 if(isset($_POST["submitBtn"])){
+                    calcScore($score);
                 }
 
-// switch ($age) {
-//     case ($age < 2):
-//         echo "Entry is Free";
-//         break;
-//     case ($age >= 2 && $age <= 12):
-//         echo "Entry is R20";
-//         break;
-//     case ($age >= 13  && $age <= 17):
-//         echo "Entry is R30";
-//         break;
-//     case ($age >= 18 && $age <= 25):
-//         echo "Entry is R40";
-//         break;
-//     case ($age >= 26  && $age <= 64):
-//         echo "Entry is R60";
-//         break;
-//     case ($age >= 65):
-//         echo "Entry is R30";
-//         break;
-//     default:
-//         echo "Please enter a valid age";
-// }
+                // calcSore Function thats takes the accumulated score variable and uses a switch statment to grade the quiz taker & display the approiate message
+
+                 function calcScore($score){
 
 
+                    switch ($score) {
+                        case ($score < 10): {
+                            $message = "Try Harder Next Time : " . $score;
+                            echo "<script type='text/javascript'>alert('$message');</script>";
+                        }
+                            break;
+                        case ($score >= 10 && $score <= 15):
+                        {
+                            $message = "Good Job , Try Harder to get a A+ : " . $score;
+                            echo "<script type='text/javascript'>alert('$message');</script>";
+                        }
+                   
+                            break;
+                        case ($score >= 16 ):
+                        {
+                            $message = "Awesome Job - A+ Score : " . $score;
+                            echo "<script type='text/javascript'>alert('$message');</script>";
+                        }
+                           break;
+                        case ($score == 4 ):
+                        {
+                            $message = "Perfect Score : " . $score;
+                            echo '<img src="images/perfectscore.gif"/>';
+                        }
+                            break;
+                        default:
+                            echo "Error , please retry quiz";
+                    }
+                    }
+                 
+            
 ?>
-       <p><input type="submit" name="Submit"></p>
+       <p><input type="submit" name="submitBtn" value="Submit for Grading"></p>
+
 </form>
  <script>
+
+// countdown timer method , modified to display a countdown timer for the quiz to auto submit for grading when timer is up 
 
   var seconds = 300;
       function secondPassed() {
@@ -123,17 +144,32 @@ $questions=array(array('What is 1+0?', '1', '2', '3', '4', '1'));
           }
 
           document.getElementById('countdown').innerHTML = minutes + ":" + remainingSeconds;
-          if (seconds == 0) {
+          
+          
+          if (seconds < 120 && seconds >=60) {
+            document.getElementById('countdown').style.color = "orange";
+          }else if (seconds < 60) {
+            document.getElementById('countdown').style.color = "red";
+          }
+          
+        var result =  " <?php echo $score ?> "; 
+
+           if (seconds == 0) {
               clearInterval(countdownTimer);
-             //form1 is your form name
-            this.form.submit();
-          } else {
+             //quizform is your form name
+              document.quizform.submitBtn.click();
+             //document.forms.quizform.submit();
+             } else {
               seconds--;
           }
+
+         
       }
       var countdownTimer = setInterval('secondPassed()', 1000);
- 
 
+    //  alert('Time Up , your score is :' + result + '');
+ 
+//used for geting the ID of any element clicked on page , was used for building the solution, to be removed
 function printID(e)
 {
     e = e || window.event;
@@ -141,6 +177,24 @@ function printID(e)
     console.log("Name: " + e.name); 
 }
 
+
+// When the user scrolls the page, execute myFunction 
+window.onscroll = function() {myFunction()};
+
+// Get the header
+var timer = document.getElementById("myTimer");
+
+// Get the offset position of the navbar
+var sticky = timer.offsetTop;
+
+// Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function myFunction() {
+  if (window.pageYOffset > sticky) {
+    timer.classList.add("sticky");
+  } else {
+    timer.classList.remove("sticky");
+  }
+}
 
  </script>
 
